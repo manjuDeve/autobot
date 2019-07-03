7067c9d4ed051878aae7ab3f6acc9913e356b8c5
581413046manju <somphong.ary@crru.ac.th>
2019-07-03 23:28:45 +0700

    Initial...

--- index.php
+++ index.php
@@ -4,21 +4,18 @@ $dbname = 'd4m7b5v2sg6snc';
 $user = 'jkgdpocorcqmzk';
 $pass = 'd41b9d3145a967b438542fc48475c08338a54f13b7c762bb4a5a0cdcbc1f2637';
 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-$resul = $connection->query("SELECT * FROM appointments  WHERE id='5' ");
 
-if($resul !== null) {
-// echo $result->rowCount();
-echo "hh";
-    while($row = $resul->fetch_assoc()) {
-        echo "id: " . $row['id']. " - Name: " . $row['time']. " " . $row['content']. "<br>";
-    }
-    echo "go";
-}
-// if ($result->num_rows > 0) {
+$sql = "SELECT id, time FROM appointments";
+$result = $connection->query($sql);
+
+if ($result->num_rows > 0) {
     // output data of each row
-   
-// } else {
-    // echo "0 results";
-// }
-$connection->close();
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["time"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
 ?>
