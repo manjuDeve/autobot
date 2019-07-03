21ecf52086c146160957e42693b8ef97122f72f9
581413046manju <somphong.ary@crru.ac.th>
2019-07-03 23:53:57 +0700

    Initial...

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
-$host = 'ec2-23-21-91-183.compute-1.amazonaws.com';
-$dbname = 'd4m7b5v2sg6snc';
-$user = 'jkgdpocorcqmzk';
-$pass = 'd41b9d3145a967b438542fc48475c08338a54f13b7c762bb4a5a0cdcbc1f2637';
-$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-$result = $connection->query("SELECT * FROM appointments ORDER BY id");
+require_once('./vendor/autoload.php');
+// Namespace
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
-if($result !== null) {
-echo $result->rowCount();
-}
-    while($row = $result->fetch()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["time"]. " " . $row["content"]. "<br>";
+$channel_token ='el8BPlEY6Cf60LnRo7mwRrdnGUT8WqsZQ03H2zdL1Xx6iGWewgjyyOhsa8Aquztz903qfCobddaA3tsmUoOySZaJ2S9VzDMgbJGsfMsGyE7OzFXxyrHPDpeawCape4Ds5VqLlkVVINwQVdIBGpt+YwdB04t89/1O/w1cDnyilFU=';
+$channel_secret = '2fbf6e976b55aea18895561298516965';
+
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+if (!is_null($events['events'])) {
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Line API send a lot of event type, we interested in message only.
+        if ($event['type'] == 'message') {
+            switch($event['message']['type']) {
+            case 'text':
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+            // Reply message
+            $respMessage = 'Hello, your message is '. $event['message']['text'];
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+            $textMessageBuilder = new TextMessageBuilder($respMessage);
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+            break;
+            }
+        }
     }
+}
+echo "OK";
