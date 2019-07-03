04693b54530566026a9b6cfb99a0b2831fd63d28
581413046manju <somphong.ary@crru.ac.th>
2019-07-04 00:09:26 +0700

    Initial...

--- index.php
+++ index.php
@@ -15,21 +15,24 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
     // Loop through each event
     foreach ($events['events'] as $event) {
-        // Line API send a lot of event type, we interested in message only.
-        if ($event['type'] == 'message') {
-            switch($event['message']['type']) {
-            case 'text':
-            // Get replyToken
-            $replyToken = $event['replyToken'];
-            // Reply message
-            $respMessage = 'Hello, your message is '. $event['message']['text'];
-            $httpClient = new CurlHTTPClient($channel_token);
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-            $textMessageBuilder = new TextMessageBuilder($respMessage);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-            break;
-            }
-        }
+    // Get replyToken
+    $replyToken = $event['replyToken'];
+    $ask = $event['message']['text'];
+    switch(strtolower($ask)) {
+    case 'm':
+    $respMessage = 'What sup man. Go away!';
+    break;
+    case 'f':
+    $respMessage = 'Love you lady.';
+    break;
+    default:
+    $respMessage = 'What is your sex? M or F';
+    break;
     }
-}
-echo "OK";
+    $httpClient = new CurlHTTPClient($channel_token);
+    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+    $textMessageBuilder = new TextMessageBuilder($respMessage);
+    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+    }
+    }
+    echo "OK";
