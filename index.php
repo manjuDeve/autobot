426e824a858f46e6ef1244b694adeae86292802c
581413046manju <somphong.ary@crru.ac.th>
2019-07-03 22:34:56 +0700

    Initial...

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo "sompong Aryeeku";
+$host = 'ec2-23-21-91-183.compute-1.amazonaws.com';
+$dbname = 'd4m7b5v2sg6snc';
+$user = 'jkgdpocorcqmzk';
+$pass = 'd41b9d3145a967b438542fc48475c08338a54f13b7c762bb4a5a0cdcbc1f2637';
+$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+$result = $connection->query("SELECT * FROM appointments");
+if($result !== null) {
+echo $result->rowCount();
+}
