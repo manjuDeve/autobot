94d94dfdf317933ecd44a586bcc4dc4728710620
581413046manju <somphong.ary@crru.ac.th>
2019-07-04 23:17:16 +0700

    Initial...

--- SELECT.php
+++ SELECT.php
@@ -5,11 +5,32 @@ $user = 'jkgdpocorcqmzk';
 $pass = 'd41b9d3145a967b438542fc48475c08338a54f13b7c762bb4a5a0cdcbc1f2637';
 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
 $result = $connection->query("SELECT * FROM appointments ORDER BY id");
-
-    if($result !== null) {
-        echo $result->rowCount();
-    }
-    while($row = $result->fetch()) {
-        echo $row["id"]. "ข้อมูล: " . $row["sec"]. " " . $row["chge"]." " . $row["bla"]. "<br>";
-    }
 ?>
+    <table class="table table-striped" border="1">
+      <tr>
+        <th >#</th>
+        <th >ชื่อหัวข้อโครงการ</th>
+        <th ></th>
+        <?php if($data['id_Schedule'] != ''){ ?>
+        <th >กำหนดสอบ</th>
+        <th >ผลการอนุมัติ</th>
+        <?php }?>
+      </tr>
+      <?php 
+      $count = 0;
+      while($row = $result->fetch()) {
+        $count++;
+        ?>
+      <tr>
+        <td><?php echo $count ?>.</td>
+        <td><?php echo $rows['sec']; ?></td>
+        <td><?php echo $rows['chge']; ?></td>
+        <td><?php echo $rows['bla']; ?></td>
+      </tr>
+      <?php } ?>  
+      <?php if($count ==0 ){ ?>
+        <tr>
+          <td colspan="6" align="center" class="mailbox-star">ไม่พบข้อมูล</td>
+        </tr>
+      <?php } ?>     
+    </table>
