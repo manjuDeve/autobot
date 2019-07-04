1669d6c47894fd47e08353caab114b691732e162
581413046manju <somphong.ary@crru.ac.th>
2019-07-04 21:54:57 +0700

    Initial...

--- SELECT.php
+++ SELECT.php
@@ -4,9 +4,9 @@ $dbname = 'd4m7b5v2sg6snc';
 $user = 'jkgdpocorcqmzk';
 $pass = 'd41b9d3145a967b438542fc48475c08338a54f13b7c762bb4a5a0cdcbc1f2637';
 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-$result = $connection->query("SELECT * FROM appointments ORDER BY id");
+$result = $connection->query("SELECT * FROM tb_laws ORDER BY id");
 
     while($row = $result->fetch()) {
-        echo $row["id"]. "ข้อมูล: " . $row["time"]. " " . $row["content"]. "<br>";
+        echo $row["id"]. "ข้อมูล: " . $row["section"]. " " . $row["Charge"]." " . $row["Blame"]. "<br>";
     }
 ?>
